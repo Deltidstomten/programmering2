4a3a1681044633c2ec13a42aa69c2025df43bd35
Deltidstomten <elias.sjoling.njord@gmail.com>
2025-09-12 08:33:02 +0200

    a

--- TEST/obj/Debug/net9.0/TEST.AssemblyInfo.cs
+++ TEST/obj/Debug/net9.0/TEST.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TEST")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+644c50f31b3bfdb5422d952771afa6f1ced2a26f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+620df5718ca7061d5319610a6365bc9dfbd35548")]
 [assembly: System.Reflection.AssemblyProductAttribute("TEST")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TEST")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
