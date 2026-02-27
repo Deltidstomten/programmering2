ad1e7f3aa83277b31fcc112d40481c53fa6d57f2
Deltidstomten <elias.sjoling.njord@gmail.com>
2026-02-27 13:15:47 +0100

    dsfa

--- TEST/obj/Debug/net9.0/TEST.AssemblyInfo.cs
+++ TEST/obj/Debug/net9.0/TEST.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TEST")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a71d7b251f37a78a334db560ae06d1288b49008a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27f7f28c0782a36e1bdf39f002cdf9e7f753dbf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TEST")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TEST")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
